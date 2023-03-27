6b158c8d34eddac8e3084d46098ad65ed4536bac
Justin West <justin.alex.west@gmail.com>
2023-03-27 10:26:08 -0700

    Wave animation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,13 +6,34 @@ export default function Home() {
   return (
     <main className={inter.className}>
       <section>
-        <div>Section 1</div>
+        <div className="wrapper">
+          <h2 className="font-bold text-lg">Section 1</h2>
+          <p className="p-20 py-10">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Expedita
+            exercitationem adipisci repudiandae ab blanditiis. Repudiandae odit
+            neque recusandae est reiciendis hic ducimus minima tenetur!
+          </p>
+        </div>
       </section>
-      <section className=" bg-sky-700 text-blue-100">
-        <div>Section 2</div>
+      <section className="wave bg-blue-600 text-blue-100">
+        <div className="wave-wrapper">
+          <h2 className="font-bold text-lg">Section 2</h2>
+          <p className="p-20 py-10">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Expedita
+            exercitationem adipisci repudiandae ab blanditiis. Repudiandae odit
+            neque recusandae est reiciendis hic ducimus minima tenetur!
+          </p>
+        </div>
       </section>
       <section>
-        <div>Section 3</div>
+        <div className="wrapper">
+          <h2 className="font-bold text-lg">Section 3</h2>
+          <p className="p-20 py-10">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Expedita
+            exercitationem adipisci repudiandae ab blanditiis. Repudiandae odit
+            neque recusandae est reiciendis hic ducimus minima tenetur!
+          </p>
+        </div>
       </section>
     </main>
   );
