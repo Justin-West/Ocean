be228e61bf839b52d6620bcaa79f2679ab5b1db2
Justin West <justin.alex.west@gmail.com>
2023-03-28 07:29:31 -0700

    Navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <main className={inter.className}>
+    <main className={inter.className + " mt-20"}>
       <section>
         <div className="wrapper">
           <h2 className="font-bold text-lg">Section 1</h2>
@@ -18,6 +18,26 @@ export default function Home() {
       <section className="wave bg-blue-600 text-blue-100">
         <div className="wave-wrapper">
           <h2 className="font-bold text-lg">Section 2</h2>
+          <p className="p-20 py-10">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Expedita
+            exercitationem adipisci repudiandae ab blanditiis. Repudiandae odit
+            neque recusandae est reiciendis hic ducimus minima tenetur!
+          </p>
+        </div>
+      </section>
+      <section>
+        <div className="wrapper">
+          <h2 className="font-bold text-lg">Section 3</h2>
+          <p className="p-20 py-10">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Expedita
+            exercitationem adipisci repudiandae ab blanditiis. Repudiandae odit
+            neque recusandae est reiciendis hic ducimus minima tenetur!
+          </p>
+        </div>
+      </section>
+      <section>
+        <div className="wrapper">
+          <h2 className="font-bold text-lg">Section 3</h2>
           <p className="p-20 py-10">
             Lorem ipsum dolor sit amet consectetur, adipisicing elit. Expedita
             exercitationem adipisci repudiandae ab blanditiis. Repudiandae odit
