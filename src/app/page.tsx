664a6586bff0a6ac760439acb83927813a3f8fc6
Justin West <39067251+Justin-West@users.noreply.github.com>
2023-03-31 14:03:59 -0700

    adding image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import { Inter } from "next/font/google";
+import Image from "next/image";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -7,11 +8,11 @@ export default function Home() {
     <main className={inter.className + " mt-20"}>
       <section className="wave">
         <div className="wave-wrapper">
-          <h2 className="font-bold text-lg">Section 1</h2>
+          <h2 className="font-bold text-lg">Welcome to the Home Page!</h2>
+          <Image src={""} alt="" />
           <p className="p-20 py-10">
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Expedita
-            exercitationem adipisci repudiandae ab blanditiis. Repudiandae odit
-            neque recusandae est reiciendis hic ducimus minima tenetur!
+            The ocean is just a thing... Nothing crazy just like the bigest
+            thing in the world!
           </p>
         </div>
       </section>
@@ -36,12 +37,12 @@ export default function Home() {
         </div>
       </section>
       <section className="wave wave700 waveStatic pt-12 bg-white text-black">
-        <div className="wave-wrapper">
-          <p className="pl-20 py-8 inline">
+        <div className="wave-wrapper text-sm text-center">
+          <p className="py-8 inline">
             All rights reserved 2023. Created by Justin West:
           </p>
           <a
-            className="pl-2"
+            className="pl-2 text-blue-800 underline underline-offset-2"
             href="https://justin-west.github.io/website-personalprofile/"
           >
             About Me
