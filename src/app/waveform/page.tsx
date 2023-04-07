e485807929a229ce43458e4ce7bb6171f8681f65
Justin West <justin.alex.west@gmail.com>
2023-04-07 07:02:18 -0700

    improvments to waveform page

--- src/app/waveform/page.tsx
+++ src/app/waveform/page.tsx
@@ -13,31 +13,48 @@ export default function Waveform() {
         <div className="wave-wrapper">
           <p className=" py-4">Enter this form to make a wave!</p>
           <form className=" text-black grid grid-cols-2 gap-2" action="">
-            <div className=" bg-black/10 p-2 rounded-lg">
+            <div className="relative bg-black/10 p-2 rounded-lg">
               <label htmlFor="" className=" text-white">
                 Name:
               </label>
-              <input className=" ml-4 rounded-md" type="text" required />
+              <input
+                className="absolute ml-auto rounded-md right-2"
+                type="text"
+                required
+              />
             </div>
 
-            <div className=" bg-black/10 p-2 rounded-lg">
+            <div className="relative bg-black/10 p-2 rounded-lg">
               <label htmlFor="" className=" text-white">
                 Name your Ocean:
               </label>
-              <input className=" ml-4 rounded-md" type="text" />
+              <input
+                className=" absolute ml-auto rounded-md right-2"
+                type="text"
+                required
+              />
             </div>
-            <div className=" bg-black/10 p-2 rounded-lg">
+            <div className="relative bg-black/10 p-2 rounded-lg">
               <label htmlFor="" className=" text-white">
                 Color of your Ocean:
               </label>
               <input
-                className=" ml-4 rounded-full bg-blue-500 p-1"
+                className="absolute ml-auto h-[24px] rounded-full w-[180px] right-2"
                 type="color"
                 name=""
                 id=""
               />
             </div>
-            <div className=" bg-black text-lg font-black px-2 rounded-md col-span-2 mx-auto w-max text-blue-500">
+            <div className="relative bg-black/10 p-2 rounded-lg">
+              <label htmlFor="" className=" text-white">
+                Saltiness:
+              </label>
+              <input
+                className=" brightness-75 contrast-200 saturate-0 absolute ml-auto rounded-md right-2 h-[24px] w-[180px]"
+                type="range"
+              />
+            </div>
+            <div className="bounce bg-black/60 text-lg font-black px-2 rounded-md col-span-2 mx-auto w-max text-blue-500 hover:bg-black hover:text-white transition-colors ">
               <input type="submit" value="Submit"></input>
             </div>
           </form>
